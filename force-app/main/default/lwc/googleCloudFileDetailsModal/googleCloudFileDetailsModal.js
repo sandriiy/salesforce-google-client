8235d68535acd03343437454446ba3106084806b
Andrii Sukhetskyi <ansukhetskyi@gmail.com>
2026-03-31 23:46:52 -0400

    #23, #24, #25: Completed/Fixed

--- force-app/main/default/lwc/googleCloudFileDetailsModal/googleCloudFileDetailsModal.js
+++ force-app/main/default/lwc/googleCloudFileDetailsModal/googleCloudFileDetailsModal.js
@@ -8,43 +8,6 @@ export default class GoogleCloudFileDetailsModal extends LightningModal {
 	@api label;
 
 	@track isLoading = true;
-	@track originalExtension;
-
-	handleFormSubmit(event) {
-		this.isLoading = true;
-		let form = this.refs.form;
-		this.validateFileExtension();
-		form.submit();
-	}
-
-	handleFormCancel(event) {
-		this.close(false);
-	}
-
-	handleFormSuccess(event) {
-		showToast(
-			this,
-			'File details saved successfully',
-			'',
-			'success'
-		);
-
-		this.close(true);
-	}
-
-	handleFormError(event) {
-		showToast(
-			this,
-			'Unable to save File Details',
-			'Please try again or contact your System Administrator',
-			'error'
-		);
-	}
-
-	handleFormLoad(event) {
-		this.isLoading = false;
-		this.extractFileExtension(event);
-	}
 
 	async handleFlowStatusChange(event) {
 		if (event.detail.status === 'STARTED') {
@@ -57,45 +20,15 @@ export default class GoogleCloudFileDetailsModal extends LightningModal {
 				'Please try again or contact your System Administrator',
 				'error'
 			);
-		}
-	}
+		} else if (event.detail.status == 'FINISHED') {
+			showToast(
+				this,
+				'File details saved successfully',
+				'',
+				'success'
+			);
 
-	extractFileExtension(event) {
-		const record = event.detail.records?.[this.localFileVersionId];
-        const originalName = record?.fields?.Name?.value;
-
-		if (originalName) {
-			const lastDotIndex = originalName.lastIndexOf('.');
-			if (lastDotIndex <= 0 || lastDotIndex === originalName.length - 1) {
-				return null;
-			}
-
-        	this.originalExtension = originalName.substring(lastDotIndex + 1);
-		}
-	}
-
-	validateFileExtension() {
-		const nameInput = this.template.querySelector(
-            'lightning-input-field[data-field="Name"]'
-        );
-
-        if (nameInput) {
-			let currentName = (nameInput.value || '').trim();
-
-			if (currentName && this.originalExtension) {
-				const lastDot = currentName.lastIndexOf('.');
-
-				let baseName;
-				if (lastDot > 0) {
-					baseName = currentName.substring(0, lastDot);
-				} else {
-					baseName = currentName;
-				}
-
-				nameInput.value = `${baseName}.${this.originalExtension}`;
-			} else {
-				nameInput.value = `Untitled.${this.originalExtension}`;
-			}
+			this.close(true);
 		}
 	}
 

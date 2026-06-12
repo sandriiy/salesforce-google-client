da6ce1ddefb9bc510eb2ce44ae67d7dbdbe334bc
Andrii Sukhetskyi <74915118+sandriiy@users.noreply.github.com>
2026-06-11 22:54:15 -0400

    #67: Add admin configuration to control OCR text in image previews (#76)

--- force-app/main/default/lwc/googleCloudMetadataConfigWizard/googleCloudMetadataConfigWizard.js
+++ force-app/main/default/lwc/googleCloudMetadataConfigWizard/googleCloudMetadataConfigWizard.js
@@ -45,6 +45,7 @@ const QUERY = gql`
                             DefaultBigFileSize__c { value }
                             OrganizationalDomain__c { value }
                             IsFilePreviewDisabled__c { value }
+                            IsImageOcrEnabled__c { value }
                             MaxDeleteChainSize__c { value }
                             CustomGoogleUploadFolderStructure__c { value }
 
@@ -81,6 +82,7 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
         organizationalDomain: '',
         defaultBigFileSize: DEFAULT_BIG_FILE_SIZE,
         isFilePreviewDisabled: false,
+        isImageOcrEnabled: false,
         maxDeleteChainSize: DEFAULT_MAX_DELETE_CHAIN_SIZE,
         customGeminiApiKey: '',
         customModelName: '',
@@ -371,6 +373,7 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
             organizationalDomain: '',
             defaultBigFileSize: DEFAULT_BIG_FILE_SIZE,
             isFilePreviewDisabled: false,
+            isImageOcrEnabled: false,
             maxDeleteChainSize: DEFAULT_MAX_DELETE_CHAIN_SIZE,
             customGeminiApiKey: '',
             customModelName: '',
@@ -391,6 +394,7 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
             organizationalDomain: extractGraphValue(recordNode?.OrganizationalDomain__c) || '',
             defaultBigFileSize: this.toNumberOrNull(extractGraphValue(recordNode?.DefaultBigFileSize__c)),
             isFilePreviewDisabled: !!extractGraphValue(recordNode?.IsFilePreviewDisabled__c),
+            isImageOcrEnabled: !!extractGraphValue(recordNode?.IsImageOcrEnabled__c),
             maxDeleteChainSize: this.toNumberOrNull(extractGraphValue(recordNode?.MaxDeleteChainSize__c)),
             customGeminiApiKey: extractGraphValue(recordNode?.CustomGeminiApiKey__c) || '',
             customModelName: extractGraphValue(recordNode?.CustomModelName__c) || '',
@@ -411,6 +415,7 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
             organizationalDomain: serverSnapshot.organizationalDomain || '',
             defaultBigFileSize: serverSnapshot.defaultBigFileSize,
             isFilePreviewDisabled: !!serverSnapshot.isFilePreviewDisabled,
+            isImageOcrEnabled: !!serverSnapshot.isImageOcrEnabled,
             maxDeleteChainSize: serverSnapshot.maxDeleteChainSize,
             customGeminiApiKey: serverSnapshot.customGeminiApiKey || '',
             customModelName: serverSnapshot.customModelName || '',
@@ -441,6 +446,7 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
         this.putIfChanged(changed, 'OrganizationalDomain__c', serverState.organizationalDomain, draftState.organizationalDomain);
         this.putIfChanged(changed, 'DefaultBigFileSize__c', serverState.defaultBigFileSize, draftState.defaultBigFileSize);
         this.putIfChanged(changed, 'IsFilePreviewDisabled__c', !!serverState.isFilePreviewDisabled, !!draftState.isFilePreviewDisabled);
+        this.putIfChanged(changed, 'IsImageOcrEnabled__c', !!serverState.isImageOcrEnabled, !!draftState.isImageOcrEnabled);
         this.putIfChanged(changed, 'MaxDeleteChainSize__c', serverState.maxDeleteChainSize, draftState.maxDeleteChainSize);
         this.putIfChanged(changed, 'CustomGoogleUploadFolderStructure__c', serverState.customGoogleUploadFolderStructure, draftState.customGoogleUploadFolderStructure);
         this.putIfChanged(changed, 'CustomGeminiApiKey__c', serverState.customGeminiApiKey, draftState.customGeminiApiKey);
@@ -461,6 +467,7 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
             organizationalDomain: draftState.organizationalDomain || '',
             defaultBigFileSize: draftState.defaultBigFileSize,
             isFilePreviewDisabled: !!draftState.isFilePreviewDisabled,
+            isImageOcrEnabled: !!draftState.isImageOcrEnabled,
             maxDeleteChainSize: draftState.maxDeleteChainSize,
             customGeminiApiKey: draftState.customGeminiApiKey || '',
             customModelName: draftState.customModelName || '',
@@ -599,7 +606,8 @@ export default class GoogleCloudMetadataConfigWizard extends LightningElement {
             (serverState.customGoogleUploadFolderStructure || '') !== (draftState.customGoogleUploadFolderStructure || '') ||
             (serverState.organizationalDomain || '') !== (draftState.organizationalDomain || '') ||
             (serverState.defaultBigFileSize ?? null) !== (draftState.defaultBigFileSize ?? null) ||
-            !!serverState.isFilePreviewDisabled !== !!draftState.isFilePreviewDisabled ||
+            (!!serverState.isFilePreviewDisabled !== !!draftState.isFilePreviewDisabled) ||
+			(!!serverState.isImageOcrEnabled !== !!draftState.isImageOcrEnabled) ||
             (serverState.maxDeleteChainSize ?? null) !== (draftState.maxDeleteChainSize ?? null) ||
             (serverState.customGeminiApiKey || '') !== (draftState.customGeminiApiKey || '') ||
             (serverState.customModelName || '') !== (draftState.customModelName || '') ||
